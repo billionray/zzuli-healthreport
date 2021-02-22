bf52bd2754a84b487d9ebc21d26c2b992c653524
billionray <8900581@qq.com>
2021-02-22 23:26:42 +0800

    适配了晨间和晚检(午检以后再说)，补完了xv的邮件

--- run.py
+++ run.py
@@ -4,9 +4,10 @@ from main import service
 from notification import mail
 datetime = time.strftime("%Y-%m-%d", time.localtime())
 datetime=datetime #日期 如果需要更改请遵循这个格式："YY-MM-DD" 
-mail_flag=0
-#try:
-#    mail_flag = os.environ['MAILFLAG']
+try:
+    mail_flag = float(os.environ['MAILFLAG'])
+except:
+    mail_flag=0
 # 经纬度查询： https://lbs.amap.com/console/show/picker
 try:
     username = os.environ['USERNAME'] 
@@ -16,6 +17,7 @@ try:
     gpslocation=os.environ['GPS'] 
     lat=float(os.environ['LAT'])
     lon=float(os.environ['LON']) 
+    reporttype=os.environ['REPORTTYPE']
 except:
     username = "" #用户名
     password= "" #密码
@@ -25,10 +27,11 @@ except:
     # 经纬度查询： https://lbs.amap.com/console/show/picker
     lat=23.23333#小数点后五位 #纬度
     lon=233.23333  #小数点后五位 #经度
+    reporttype="" #home/dawn/night
 #以下可选
 try:
-    my_user=os.environ['USER']
-    my_sender=os.environ['SENDER']
+    my_user=os.environ['MYUSER']
+    my_sender=os.environ['MYSENDER']
     SMTPdomain=os.environ['SMTPDOMAIN']
     SMTPauth=os.environ['SMTPAUTH']
 except:
@@ -36,7 +39,7 @@ except:
     my_sender="" #发件人
     SMTPdomain="" #发件人SMTP地址（SSL）
     SMTPauth="" #发件人SMTP授权码
-run=service(username,password,mobile,homemobile,gpslocation,lat,lon,datetime)
+run=service(username,password,mobile,homemobile,gpslocation,lat,lon,datetime,reporttype)
 
 if run==1:
    reportstatus=1 #这里是为了以后方便加入retry和其它通知方式
@@ -44,7 +47,8 @@ if run==1:
 else:
    reportstatus=0
    print("faild")
-if mail==1:
+
+if mail_flag==1:
     if reportstatus==1:
         mail("成功",my_user,my_sender,SMTPdomain,SMTPauth,datetime)
     else:
