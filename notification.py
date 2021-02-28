0765bfeb7647cd940c0fa3d033997a138df450bd
billionray <8900581@qq.com>
2021-02-28 22:19:43 +0800

    还原notification

--- notification.py
+++ notification.py
@@ -8,7 +8,7 @@ def mail(yesorno,my_user,my_sender,SMTPdomain,SMTPauth,datetime):
         msg = MIMEText(user_dict.get("user_code", ) + ':' + '打卡' + yesorno + '！', 'plain', 'utf-8')
         msg['From'] = formataddr(["打卡提醒", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
         msg['To'] = formataddr(["您好，订阅者", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-        msg['Subject'] = datetime+reporttype+"打卡"+yesorno+username  # 主题
+        msg['Subject'] = datetime+"打卡"+yesorno  # 主题
         server = smtplib.SMTP_SSL(SMTPdomain, 465)  # 使用SSL发送
         server.login(my_sender, SMTPauth)  # SMTP密码，这里是我的的密码
         server.sendmail(my_sender, [my_user, ], msg.as_string())
