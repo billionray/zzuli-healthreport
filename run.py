d0216b29eb0505b4a45f1037f46e8d0511a487f6
billionray <23333333333@qq.com>
2021-08-07 12:24:31 +0800

    update run.py

--- run.py
+++ run.py
@@ -44,11 +44,11 @@ elif(nowtime>20 and nowtime <24):
     reporttype = "dorm"
 
 try:
-    home=float(os.environ['HOME'])
+    home=os.environ['HOME']
 except:
     home=load_dict.get("home", )
    # 将其改为1启用居家打卡
-if (home) == 1:
+if (home) == "1":
     reporttype = "home"
 
 
