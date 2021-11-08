ff5cd5e416efb06692c6c60952a1307643ea6f7d
skyiea314 <49555328+skyiea314@users.noreply.github.com>
2021-11-08 18:23:21 +0800

    Add comment

--- app/login.py
+++ app/login.py
@@ -4,9 +4,8 @@ import json
 import base64
 from .data import data_generate
 
-binPwd = password.encode('utf-8')
-base64Pwd = base64.b64encode(binPwd).decode('utf-8')
-session=HTMLSession()
+# 常量
+url = "http://kys.zzuli.edu.cn/cas/login"
 getHeader = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Encoding": "gzip, deflate",
@@ -17,14 +16,6 @@ getHeader = {
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.18 Safari/537.36"
 } 
-
-url = "http://kys.zzuli.edu.cn/cas/login"
-getResponse = session.get(url, headers = getHeader)
-postCookie = getResponse.cookies
-print(postCookie)
-lt = getResponse.html.find("[name=lt]")
-execution = getResponse.html.find("[name=execution]")
-print()
 postHeader={
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Encoding": "gzip, deflate",
@@ -39,12 +30,20 @@ postHeader={
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.18 Safari/537.36"
 }
-data = "username="+username+"&password={base64}_"+base64Pwd+"&secret=&lt="+lt[0].attrs["value"]+"&execution="+execution[0].attrs["value"]+"&_eventId=submit"
-print(data)
+session=HTMLSession()
+base64Pwd = base64.b64encode(password.encode('utf-8')).decode('utf-8')
+# 在登录前进行get，获取html和cookie
+getResponse = session.get(url, headers = getHeader)
+postCookie = getResponse.cookies
 
-postResponse = session.post(url=url, headers=postHeader, cookies=postCookie, data=data)
+# 获取请求体中的lt和execution 
+# TODO:加上判空条件 
+lt = getResponse.html.find("[name=lt]") 
+execution = getResponse.html.find("[name=execution]")
 
+# 拼接请求体 并发送post请求获取cookie
+payload = "username="+username+"&password={base64}_"+base64Pwd+"&secret=&lt="+lt[0].attrs["value"]+"&execution="+execution[0].attrs["value"]+"&_eventId=submit"
+postResponse = session.post(url=url, headers=postHeader, cookies=postCookie, data=payload)
 userCookie = postResponse.cookies
 userHeader = postResponse.headers
-print(userCookie)
-print(userHeader)
+
