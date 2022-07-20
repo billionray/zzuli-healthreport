2c2548e033474370f8215d0f7e64b64ba80c9ae7
十有** <12987201+billionray@users.noreply.github.com>
2022-07-20 15:45:06 +0800

    适配2022/7/19表单变动

--- app/data.py
+++ app/data.py
@@ -25,7 +25,7 @@ def data_generate(get_user_url, headers, cookies, proxies, debug, report_type, d
     schoollat = data[10]
     schoollon = data[11]
     vaccine = data[12]
-    nucleicacidtest = “”
+    nucleicacidtest = data[13]
     lasttest = data[14]
     novaccine_reason=data[15]
     novaccine_detail=data[16]
@@ -91,7 +91,7 @@ def data_generate(get_user_url, headers, cookies, proxies, debug, report_type, d
         "yjs": 0,
         "other": "无",
         "jjymqk": vaccine,
-        "hsjcqk": nucleicacidtest,
+        "hsjcqk": “”,
         "no_yy":novaccine_reason,
         "no_jtyy":novaccine_detail,
         "last_time": datetime,
